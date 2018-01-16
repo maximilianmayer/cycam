482868c17c0d832fc2447f2d2afaa28a16229841
Maximilian Mayer <maximilian.mayer@sixt.com>
2018-01-16 19:00:06 +0100

    first draft of using kraken api

--- app.rb
+++ app.rb
@@ -1,13 +1,29 @@
 require 'net/http'
 require 'sinatra'
-#require 'sinatra/json'
 require 'tilt'
 require 'tilt/erb'
 require 'json'
 require 'yaml'
+require 'kraken_client'
 
-def get_trades ; end
+module Cycam
 
-get '/' do
-  erb :index
+  class Webapp < Sinatra::Base
+    KrakenClient.configure do |config|
+      config.api_key = '5zqVAsOdHiZK5sV7WPdRX4si4z/CRSSO6FgKhpeIu9/pVfkKQz1oX1FE'
+      config.api_secret = 'U0SzR3KAmEUAUcgHxWQMPmYMwlsLktD7F9zBRt8D393qch9WZV5jalkklu48j0QWtdbBLE5t9apxVfyo0G7KDg=='
+    end
+
+    kraken = KrakenClient.load
+    #def get_trades ; end
+
+    get '/' do
+      trades = kraken.private.trades_history['trades']
+          #balance: 0.0, # kraken.private.balance,
+      puts trades.class
+      puts trades
+      erb :index, locals: { trades: trades }
+    end
+  end
+
 end
---
+++ config.ru
@@ -0,0 +1,3 @@
+require './app'
+
+Cycam::Webapp.run!
